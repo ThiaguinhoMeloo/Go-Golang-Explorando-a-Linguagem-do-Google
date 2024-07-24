070bb31bb43c4967928dc80a83c83852ed33e536
Thiago Melo <jamesmelo329@gmail.com>
2024-07-24 16:04:45 -0300

    feat: Add information to the struct and func main of the struct

--- inicio/systems_and_types/struct.go
+++ inicio/systems_and_types/struct.go
@@ -11,3 +11,20 @@ type Product struct {
 func (p Product) DiscountedProduct() float64 {
 	return p.Price * (1 - p.Discount)
 }
+
+//
+// var product1 systemsandtypes.Product
+// product1 = systemsandtypes.Product{
+// 	Name:     "Lapis",
+// 	Price:    1.79,
+// 	Discount: 0.05,
+// }
+// fmt.Println(product1, product1.DiscountedProduct())
+
+// product2 := systemsandtypes.Product{
+// 	"Notebook",
+// 	1989.90,
+// 	0.10,
+// }
+
+// fmt.Println(product2.DiscountedProduct())
