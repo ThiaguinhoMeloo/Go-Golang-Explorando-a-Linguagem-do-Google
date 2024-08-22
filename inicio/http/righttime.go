49d7c8255ab8b9f6e136d2cf4b12ae1b6723ebd0
Thiago Melo <thiagomelo@itstransdata.com>
2024-08-22 10:48:26 -0300

    refactor: righttime.go

--- inicio/http/righttime.go
+++ inicio/http/righttime.go
@@ -1,24 +1,33 @@
 package http
 
 import (
-	"fmt"
 	"net/http"
+	"text/template"
 	"time"
 )
 
-func RightTimeV2(w http.ResponseWriter, r *http.Request) {
-	s := time.Now().Format("02/01/2006 15:04:05")
-	fmt.Fprint(w, s)
+var tpl *template.Template
+
+type Hora struct {
+	Hora string
 }
 
-func ServeTemplate(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "http/hora/index.html")
+func CurrentTime(w http.ResponseWriter, r *http.Request) {
+	// http.ServeFile(w, r, "http/hora/index.html")
+	tpl, _ = template.ParseFiles("http/hora/index.html")
+	loc, _ := time.LoadLocation("America/Sao_Paulo")
+	now := time.Now().UTC().In(loc)
+	data := Hora{
+		Hora: now.Format("2006-01-02 15:04:05"),
+	}
+
+	err := tpl.Execute(w, data)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
 }
 
-// fs := http.FileServer(http.Dir("http/hora"))
-// http.Handle("/http/", http.StripPrefix("/http/", fs))
-// http.HandleFunc("/horaCerta", request.RightTimeV2)
-// http.HandleFunc("/", request.ServeTemplate)
-
-// log.Println("Executando...")
-// log.Fatal(http.ListenAndServe(":3000", nil))
+// func main() {
+// 	http.HandleFunc("/", request.ServeTemplate)
+// 	http.ListenAndServe(":3000", nil)
+// }
--- inicio/main.go
+++ inicio/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	request "inicio/http"
+	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	request.Static()
+	http.HandleFunc("/", request.CurrentTime)
+	http.ListenAndServe(":3000", nil)
 }
